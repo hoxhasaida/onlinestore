4466f565bf4aa66a9b066bea8fbfc9f31d5ac2e6
Saida <saida@siliconstack.com.au>
2019-05-25 12:08:27 +1000

    Add project files.

---
+++ wwwroot/js/app/index.js
@@ -0,0 +1,10 @@
+﻿$(function () {
+    var app = new Vue({
+        el: '#app',
+        methods: {
+            viewProducts: function () {
+                $("#modal-products").modal("show");
+            }
+        }
+    });
+});
